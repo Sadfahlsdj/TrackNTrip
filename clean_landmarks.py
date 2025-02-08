41fede8f9633d631f4fd1576c3c98106bdf74bdb
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-02-07 22:47:42 -0500

    updated file to clean the landmarks added csv file for correct structuring added api endpoint to grab data regarding a landmark given that landmark

--- clean_landmarks.py
+++ clean_landmarks.py
@@ -4,8 +4,9 @@ df = pd.read_csv('landmarks_data/landmarks_boston.csv')
 
 df['lat_lon'] = [d.splitlines()[-1] for d in df['Location']]
 df['address'] = [' '.join(d.splitlines()[:-1]) for d in df['Location']]
-df['Date designated'] = [d.splitlines()[0] for d in df['Date designated']]
+df['Date'] = [d.splitlines()[0] for d in df['Date designated']]
+df['name'] = [n.lower() for n in df['Landmark name']]
 
-df = df.drop(['Image', 'Location'], axis=1)
-df.to_csv('landmarks_boston_cleaned.csv')
+df = df.drop(['Image', 'Location', '[2]', 'Landmark name', 'Date designated'], axis=1)
+df.to_csv('landmarks_data/landmarks_boston_cleaned.csv', index=False)
 print(df.head().to_string())
---
+++ landmarks_api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.get('/info')
+def get_info():
+    """
+    api arguments:
+        city - city name (only supports boston right now)
+        landmark - name of landmark (needs to match case insensitive exactly for now)
+
+    :return: json with date, description, latitude longitude data, address
+
+    example usage: http://127.0.0.1:5000/info?city=boston&landmark=African%20Meeting%20House
+    """
+    city = request.args.get('city').lower()
+    landmark = request.args.get('landmark').lower()
+
+    path = f'landmarks_data/landmarks_{city}_cleaned.csv'
+    df = pd.read_csv(path)
+    out = df[df['name'] == landmark][['Date', 'Description', 'lat_lon', 'address']]
+
+    return out.to_json(orient='records')
