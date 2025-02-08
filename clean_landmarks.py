4b16ec63d400b4326d3dc57cf12c1f01ac39ad75
Sadfahlsdj's mac <124646333+Sadfahlsdj@users.noreply.github.com>
2025-02-08 00:57:15 -0500

    reformatted latitude & longitude in landmarks_boston_cleaned.csv; added file that did so

--- clean_landmarks.py
+++ clean_landmarks.py
@@ -9,5 +9,23 @@ df['Date'] = [d.splitlines()[0] for d in df['Date designated']]
 df['name'] = [n.lower() for n in df['Landmark name']]
 
 df = df.drop(['Image', 'Location', '[2]', 'Landmark name', 'Date designated'], axis=1)
+
+# reformat lat lon away from DMS to something we can use more easily
+lat_lon = []
+for l in df['lat_lon']:
+    d = int(l.split('.')[0])
+    m = float(int(l.split('.')[1][:2]) / 60)
+    s = float(int(l.split("'")[1][:2]) / 3600)
+    NS = d + m + s
+
+    l = l.split(' ')[1]
+    d = int(l.split('.')[0])
+    m = float(int(l.split('.')[1][:2]) / 60)
+    s = float(int(l.split("'")[1][:2]) / 3600)
+    WE = d + m + s
+
+    lat_lon.append(f'{NS};{WE}')
+
+df['lat_lon'] = lat_lon
 df.to_csv('landmarks_data/landmarks_boston_cleaned.csv', index=False)
-print(df.head().to_string())
+print(df['lat_lon'])
